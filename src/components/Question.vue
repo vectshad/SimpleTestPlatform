02c48f4e17d55c38c82ee1e80e4cc8adfeba8448
ekasunandika <13517130@std.stei.itb.ac.id>
2020-04-24 17:32:19 +0700

    add question with multiple choice

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,13 +1,32 @@
 <template>
+<div>
   <div class="question-box">
-    {{ question }}
+    {{ questionDesc}}
   </div>
+  <div class="question-answer">
+    <div class="question">
+      <p> {{ question }} </p>
+    </div>
+    <div class="answer">
+      <form>
+        <input type="radio" name="choice" value="Scripting"> Scripting
+        <br>
+        <input type="radio" name="choice" value="Programming"> Programming
+        <br>
+        <input type="radio" name="choice" value="Application"> Application
+        <br>
+        <input type="radio" name="choice" value="None of These"> None of These
+      </form>
+    </div>  
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   name : "Question",
   props: {
+    questionDesc: String,
     question: String
   }
 }
@@ -23,5 +42,14 @@ export default {
   padding: 20px;
   margin: 20px;
   text-align: left;
+}
+
+.question-answer {
+  margin: 20px;
+  width: 650px;
+  text-align: left;
+}
+.p.question {
+  font-weight: bold;
 }
 </style>
