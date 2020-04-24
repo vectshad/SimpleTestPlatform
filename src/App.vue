75313ca92a433f0915405196909063e8534b1c8c
ekasunandika <13517130@std.stei.itb.ac.id>
2020-04-25 03:37:23 +0700

    create page for questions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <Question :questionDesc="questionText" :question="quest" :answer="answerList" />
+    <div class="row">
+      <div class="column">
+        {{ title }} 
+      </div>
+      <div class="column">
+        <a class="round" @click="changePage('prev')"> &#8249;</a> 
+        {{ page }}
+        <a class="round" @click="changePage('next')"> &#8250; </a>
+      </div>  
+    </div>
+    <div>
+      <Question :questionDesc="questionText" :question="quest" :answer="answerList" />
+    </div>
   </div>
 </template>
 
@@ -13,8 +25,14 @@ export default {
   components: {
     Question
   },
+  props: {
+    title: {
+      default: "TPS - Penalaran Umum",
+    }
+  },
   data () {
     return {
+      page: 1,
       questionText : "",
       quest: "",
       answerList: [],
@@ -36,6 +54,13 @@ export default {
         console.log(err);
       })
       console.log(this.payloads)
+    },
+    changePage (event) {
+      if (event === "next" ) {
+        this.page = this.page + 1
+      } else if (event === "prev" && this.page > 1) {
+        this.page = this.page - 1
+      }
     }
   }
 }
@@ -49,5 +74,45 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.row {
+	margin:20px;
+	text-align:left;
+	width:100%;
+	font-size:0.1px;/* Kill white-space*/
+}
+.column {
+	display:inline-block;
+	padding:5px;
+	vertical-align:top;
+	font-size:22px;
+  margin-right: 400px;
+}
+.row:after{
+	content:"";
+	display:inline-block;
+	height:1px;
+	width:0;
+	font-size:0;
+	line-height:0;
+	overflow:hidden;
+	margin: 0 100% 0 0;
+}
+
+a {
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 16px;
+  background-color: #ddd;
+}
+
+a:hover {
+  background-color: #4CAF50;
+  color: black;
+}
+
+.round {
+  border-radius: 50%;
 }
 </style>
